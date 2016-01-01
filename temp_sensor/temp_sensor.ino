921c6a748cb8b98f0dbac4b48ddd3f8b9b8b7639
Gily Merkado <gilymerkado@gmail.com>
2016-01-01 16:45:45 +0200

    Changed Arduino code to use a single one wire temperature sensor (pin 2).

--- temp_sensor/temp_sensor.ino
+++ temp_sensor/temp_sensor.ino
@@ -1,4 +1,9 @@
-const int temperaturePin = 0;
+#include <OneWire.h>
+
+const int temperaturePin = 2;
+
+//Temperature chip i/o
+OneWire ds(temperaturePin); // on digital pin 2
 
 void setup()
 {
@@ -8,25 +13,45 @@ void setup()
 
 void loop()
 {
-  float voltage, degreesC, degreesF;
-  voltage = getVoltage(temperaturePin);
-  degreesC = (voltage - 0.5) * 100.0;
-  degreesF = degreesC * (9.0/5.0) + 32.0;
- 
-  //Serial.print("voltage: ");
-  //Serial.print(voltage);
-  //Serial.print("  deg C: ");
-  Serial.println(degreesC);
-  //Serial.print("  deg F: ");
-  //Serial.println(degreesF);
+  float temperature = getTemp();
+  Serial.println(temperature);
+  delay(1000); // repeat once per second
+}
+
+float getTemp(){
+  //returns the temperature from one DS18S20 in DEG Celsius
   
-  delay(1000); // repeat once per second (change as you wish!)
+  byte data[12];
+  byte addr[8];
+  
+  if(!ds.search(addr)) {
+    //no more sensors on chain, reset search
+    ds.reset_search();
+    return -1000;
+  }
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+    Serial.print("Device is not recognized");
+    return -1000;
+  }
+  
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44, 1);//Start conversion, with parasite power on at the end
+  byte present = ds.reset();
+  ds.select(addr);
+  ds.write(0xBE);// Read Scratchpad
+  
+  for (int i = 0; i < 9; i++) { // we need 9 bytes
+    data[i] = ds.read();
+  }
+  
+  ds.reset_search();
+  
+  byte MSB = data[1];
+  byte LSB = data[0];
+  
+  float tempRead = ((MSB << 8) | LSB); //using two's compliment
+  float TemperatureSum = tempRead / 16;
+  
+  return TemperatureSum;
 }
-
-
-float getVoltage(int pin)
-{
-  return (analogRead(pin) * 0.004882814);
-}
-
-
